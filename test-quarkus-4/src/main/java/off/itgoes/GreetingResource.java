b2c1f92b248217b188031087cab56b4d12d83269
campesinux <corrado.campisano@gmail.com>
2024-12-09 10:18:26 +0100

    implementato quinto tutorial, che prosegue nel quarto, come fara' il sesto

--- test-quarkus-4/src/main/java/off/itgoes/GreetingResource.java
+++ test-quarkus-4/src/main/java/off/itgoes/GreetingResource.java
@@ -1,5 +1,8 @@
 package off.itgoes;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -8,9 +11,13 @@ import jakarta.ws.rs.core.MediaType;
 @Path("/hello")
 public class GreetingResource {
 
+	@Inject
+	@ConfigProperty(name="quarkus.application.name", defaultValue="my-service")
+	private String applicationName;
+	
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from Quarkus REST";
+        return "Hello from Quarkus REST, on " + applicationName;
     }
 }
